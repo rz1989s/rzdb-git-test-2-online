5d71eca3aa41ecc487a410c61229dafda8451333
rz1989s <rheza10@gmail.com>
2022-01-17 16:46:42 +0700

    Mengedit index.html

--- index.php
+++ index.php
@@ -9,5 +9,6 @@
 <body>
     <h1>GIT remote RZDB - push</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora sint ab suscipit dolore, repellat, inventore quis facere debitis harum atque iure aliquam animi totam commodi libero unde tempore necessitatibus nemo.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non molestiae consectetur ea asperiores a. Ut eius quidem iste voluptatem pariatur amet voluptas qui iure, cumque voluptate aperiam at provident velit.</p>
 </body>
 </html>
